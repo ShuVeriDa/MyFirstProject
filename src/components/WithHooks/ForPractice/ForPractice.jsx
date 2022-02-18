18b5dfb4e39b2f4aeab44d45e34e659b6b3743f0
ShuVeriDA <bashtarov@outlook.com>
2022-02-18 06:58:58 +0300

    practic checked

--- src/components/WithHooks/ForPractice/ForPractice.jsx
+++ src/components/WithHooks/ForPractice/ForPractice.jsx
@@ -2,40 +2,19 @@ import { useState } from "react"
 import classes from './ForPractice.module.css'
 
 const ForPractice = () => {
-  const [name, setName] = useState('Введите ваше имя')
-  const [lastName, setLastName] = useState('Введите вашу фамилию')
-  const [age, setAge] = useState('Введите ваш возраст')
-  const [ban, setBan] = useState(true)
+ const [checked, setChecked] = useState(false)
 
-  const clickHandlerName = () => {
-    setName('Muhammad')
-  } 
-
-  const clickHandlerLastName = () => {
-    setLastName('Bashtarov')
-  } 
-
-  const clickHandlerAge = () => {
-    setAge(27)
-  } 
-
-
-
+ let message;
+ if (checked) {
+   message = <p>Здравствуйте</p>
+ } else {
+  message = <p>Прощайте</p>
+ }
+ 
   return (
     <div className={classes.ForPractice}>
-      <p>Name: {name} <button onClick={clickHandlerName}>Change Name</button></p>
-      <p>LastName: {lastName} <button onClick={clickHandlerLastName}>Change LastName</button></p>
-      <p>
-        Age: {age}  
-        <button onClick={clickHandlerAge}>Change Age</button>
-        <button onClick={() => setAge(age + 1)}>+1</button>
-        <button onClick={() => setAge(age - 1)}>-1</button>
-      </p>
-      <button onClick={() => setBan(false)}>Забанить</button> 
-      <button onClick={() => setBan(true)}>Разбанить</button>
-      <p>
-        Пользователь: {name} {lastName} - {ban ? 'Разбанен' : "ЗАБАНЕН"}
-      </p>
+      <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
+      <p>{message}</p>
     </div>
   )
 }
