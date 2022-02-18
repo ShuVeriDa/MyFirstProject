1e248d8ea26145eb90a670c87d5e2ceb4b63568a
ShuVeriDA <bashtarov@outlook.com>
2022-02-18 07:02:58 +0300

    practic checked2

--- src/components/WithHooks/ForPractice/ForPractice.jsx
+++ src/components/WithHooks/ForPractice/ForPractice.jsx
@@ -2,19 +2,24 @@ import { useState } from "react"
 import classes from './ForPractice.module.css'
 
 const ForPractice = () => {
- const [checked, setChecked] = useState(false)
-
- let message;
- if (checked) {
-   message = <p>Здравствуйте</p>
- } else {
-  message = <p>Прощайте</p>
- }
+  const [checked, setChecked] = useState(false)
+  let message
+  if (checked) {
+    message = <div>
+      <h2>Ура, вам уже есть 18</h2>
+      <p>здесь расположен контент только для взрослых</p>
+    </div>
+  } else {
+    message = <div>
+      <p>Увы, вам еще нет 18:(</p>
+    </div>
+  }
  
   return (
     <div className={classes.ForPractice}>
-      <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
-      <p>{message}</p>
+      <p>"Вам действительно 18 лет?"</p>
+      <input type="checkbox" onChange={() => setChecked(!checked)}/>
+      {message}
     </div>
   )
 }
