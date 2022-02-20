df6d190bd0ac4ddc651ec2492da56fef9754f78d
ShuVeriDA <bashtarov@outlook.com>
2022-02-20 19:55:49 +0300

    practic Binding inputs to an array in React

--- src/components/WithHooks/ForPractice/ForPractice.jsx
+++ src/components/WithHooks/ForPractice/ForPractice.jsx
@@ -2,40 +2,39 @@ import {useState} from "react"
 import classes from './ForPractice.module.css'
 
 const ForPractice = () => {
-    const [notes, setNotes] = useState(['a', 'b', 'c', 'd', 'e'])
-    const [value, setValue] = useState("")
+    const [notes, setNotes] = useState([1, 2, 3])
+
+    const getSum = (arr) => {
+        let sum = 0
+
+        for (let elem of arr) {
+            sum += +elem
+        }
+        return sum
+    }
+
+    const changeHandler = (index, event) => {
+        setNotes([...notes.slice(0, index), event.target.value, ...notes.slice(index + 1)])
+    }
 
     const result = notes.map((note, index) => {
         return (
-            <li key={index}>
-                {note}
-                <button onClick={() => remItem(index)}>RemoveNote</button>
-            </li>
+            <div>
+                <input
+                    key={index}
+                    value={note}
+                    onChange={(event) => changeHandler(index, event)}
+                />
+            </div>
 
         )
     })
 
-    const handleInput = (event) => {
-        setValue(event.target.value)
-    }
-
-    const addItem = () => {
-        setNotes([...notes, value])
-        setValue('')
-    }
-
-    const remItem = (index) => {
-        setNotes([...notes.slice(0, index), ...notes.slice(index + 1)])
-    }
-
     return (
         <div className={classes.forPractice}>
-            <ul>
-                {result}
-            </ul>
+            {result}
 
-            <input value={value} onChange={handleInput}/>
-            <button onClick={addItem}>Add</button>
+            {getSum(notes)}
         </div>
     )
 }
