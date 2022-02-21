b423caed35af84dc5afd129168f95785d9d4829b
ShuVeriDA <bashtarov@outlook.com>
2022-02-21 21:01:01 +0300

    edited practic

--- src/components/WithHooks/ForPractice/ForPractice.jsx
+++ src/components/WithHooks/ForPractice/ForPractice.jsx
@@ -2,10 +2,47 @@ import {useState} from "react"
 import classes from './ForPractice.module.css'
 
 const ForPractice = () => {
+    const [notes, setNotes] = useState([1, 2, 3, 4, 5])
+    const [editNum, setEditNum] = useState(null)
+    const [value, setValue] = useState('')
+
+    const result = notes.map((note, index) => {
+        return <p key={index} onClick={() => startEdit(index)}>{note}</p>
+    })
+
+    const startEdit = (index) => {
+        setEditNum(index)
+        setValue(notes[editNum])
+    }
+
+    const changeHandler = (event) => {
+        setValue(event.target.value)
+
+        if (editNum) {
+            setNotes([...notes.slice(0, editNum), event.target.value, ...notes.slice(editNum + 1)])
+        } else {
+            setEditNum(null)
+        }
+
+        setValue('')
+    }
+
+    const blurHandler = (event) => {
+        if (!editNum) {
+            setNotes([...notes, value])
+        } else {
+
+        }
+    }
 
     return (
         <div className={classes.forPractice}>
-            ForPractice
+            {result}
+            <input
+                value={value}
+                onChange={changeHandler}
+                onBlur={blurHandler}
+            />
         </div>
     )
 }
